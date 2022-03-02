9c0b180df7e575cddaf5d4c59aa8800e9fceba1a
Devon Faria <devonfaria@gmail.com>
2022-03-02 15:37:29 -0500

    added functions to show or hide HTML elements and tested them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// DEFINING ELEMENT SELECTORS
 var startScreen = document.querySelector('.start-screen');
 var questionScreen = document.querySelector('.question-screen');
 var endScreen = document.querySelector('.end-screen');
@@ -7,3 +8,33 @@ var answerButton = document.querySelector('.answer-button');
 var submitButton = document.querySelector('.submit-button');
 var clearHighscoresButton = document.querySelector('.clear-highscores-button');
 var initialsInput = document.querySelector('.initials-input');
+
+// DEFINING FUNCTIONS
+// Show start screen
+var showStart = function () {
+  startScreen.style.display = 'block';
+  questionScreen.style.display = 'none';
+  endScreen.style.display = 'none';
+  highscoreScreen.style.display = 'none';
+}
+// Show question screen
+var showQuestion = function () {
+  startScreen.style.display = 'none';
+  questionScreen.style.display = 'block';
+  endScreen.style.display = 'none';
+  highscoreScreen.style.display = 'none';
+}
+// Show end screen
+var showEnd = function () {
+  startScreen.style.display = 'none';
+  questionScreen.style.display = 'none';
+  endScreen.style.display = 'block';
+  highscoreScreen.style.display = 'none';
+}
+// Show highscores screen
+var showHighscores = function () {
+  startScreen.style.display = 'none';
+  questionScreen.style.display = 'none';
+  endScreen.style.display = 'none';
+  highscoreScreen.style.display = 'block';
+}
